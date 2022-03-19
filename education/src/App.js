32042386d7ffb9c5a055f41954f80251dc9d3a98
jharaghav <raghavkr1000@gmail.com>
2022-03-19 11:31:47 +0530

    created an react app

---
+++ education/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './component/Navbar';
+import Signup from './component/Student/Signup';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Signup/>
+    </>
+  );
+}
+
+export default App;
